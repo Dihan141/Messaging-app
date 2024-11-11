4c1830982e2870d35391803de0cbc23d72c5e3d3
Dihan141 <87206556+Dihan141@users.noreply.github.com>
2024-11-11 19:36:41 +0600

    basic socket io setup

--- client/src/components/ChatWindow.jsx
+++ client/src/components/ChatWindow.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import io from 'socket.io-client'
 
 function ChatWindow() {
+  const backendUrl = import.meta.env.VITE_BACKEND_URL
+  const socket = io(backendUrl)
+
+  const [messages, setMessages] = useState([])
+  const [input, setInput] = useState('')
+
+  useEffect(() => {
+    socket.on('message', (message) => {
+      console.log('Message received:', message)
+      setMessages((prevMessages) => [...prevMessages, message]);
+    })
+
+    return () => {
+      socket.off('message')
+    }
+  },[])
+
+  const sendMessage = () => {
+    if(input.trim()){
+      socket.emit('message', input)
+      setInput('')
+    }
+  }
+
   return (
     <div className='chat-window'>          
         <h2>Chat with xyz</h2>
         <div className="chat-messages">
         {/* Here you can render message history or live chat messages */}
+          {messages.map((msg, index) => (
+              <p key={index}>{msg}</p>
+          ))}
         </div>
-        <input type="text" placeholder="Type a message..." className="message-input" />
+        <input type="text" placeholder="Type a message..." className="message-input" value={input} onChange={(e) => setInput(e.target.value)}/>
+        <button onClick={sendMessage} >Send</button>
     </div>
   )
 }
--- server/server.js
+++ server/server.js
@@ -3,14 +3,24 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const { createServer } = require('http')
+const { Server } = require('socket.io') 
 const app = express()
+const server = createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5173', // Allow only your React app's origin
+        methods: ['GET', 'POST'],
+        credentials: true
+    }
+})
 
 //routes
 const authroutes = require('./user/routers/authRoutes');
 const userRoutes = require('./user/routers/userRoutes');
 
 app.use(cors({
-    origin: '*'
+    origin: 'http://localhost:5173'
 }));
 app.use(express.json())
 
@@ -28,6 +38,19 @@ mongoose.connect(process.env.MONGO_URI).then(()=>{
 app.use('/api/auth', authroutes)
 app.use('/api/user', userRoutes)
 
-app.listen(process.env.PORT, ()=>{
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
+    socket.on('message', (data) => {
+        console.log('Message received:', data);
+        io.emit('message', data); // Broadcast the message to all clients
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+})
+
+server.listen(process.env.PORT, ()=>{
     console.log(`Server is running on port ${process.env.PORT}`)
 })
